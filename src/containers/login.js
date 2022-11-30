40ab7485aba2ca92edb41502a46a10adea1ec11a
anil-bhusal <anilbhusal0904@gmail.com>
2022-11-30 22:00:58 +0545

    props mod

--- src/containers/login.js
+++ src/containers/login.js
@@ -1,8 +1,18 @@
 import Button from "../components/button"
 
 function Login() {
+  const arr = ['anil', 'ashok', 'ram']
+  const color = "green"
+
+  // const [style, setStyle] = {color}
+
   return (
 <>
+{
+  arr.map((item, id) => {
+    return <li style={{color: color}}> item</li>
+  })
+}
 <Button value = {"login"} bcolor = {"green"} color ={"white"}/>
 </>
   );
